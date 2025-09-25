3ebba8bd10517c026b5e036d8bfd132671b3b4ca
isac hayson <isakraison@gmail.com>
2025-09-25 09:29:19 -0300

    novo exemplo da aula

---
+++ Aula_poo.py
@@ -0,0 +1,39 @@
+class Carro:
+    def __init__(self,marca,modelo,ano,cor):
+     self.marca=marca 
+     self.modelo=modelo 
+     self.ano=ano 
+     self.cor=cor
+     self.velocidade=0
+
+    def acelerar(self,valor):
+        self.velocidade+=valor
+        print(f"{self.modelo}acelerou para {self.velocidade}km/h")
+
+    def frear(self,valor):
+        self.velocidade-=valor
+        if self.velocidade<0:
+            self.velocidade=0
+            print(f"{self.modelo}reduziu para {self.velocidade}km/h.")
+
+    def detalhes(self):
+        return(f"{self.marca}{self.modelo}({self.ano}) - "
+               f"cor:{self.cor},velocidade: {self.velocidade} km/h.")
+    
+carro1 = Carro("toyota","corolla",2020,"preto")
+carro2 = Carro("honda","civic",2019,"vermelho")
+
+print(carro1.detalhes())
+print(carro2.detalhes())
+
+carro1.acelerar(50)
+carro2.acelerar(30)
+
+carro1.frear(20)
+carro2.frear(15)
+
+print(carro1.detalhes())
+print(carro2.detalhes())
+
+     
+    
